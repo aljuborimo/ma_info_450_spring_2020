4e576fc0041dc34c7b4625ab5aeaa46944f3e91b
aljuborimo <58051833+aljuborimo@users.noreply.github.com>
2020-01-23 16:45:11 -0500

    Add files via upload

---
+++ chapter2/program3.cpp
@@ -0,0 +1,17 @@
+/*  
+    Mohammed aljubori 
+    Info 450, Professor Fauerbach
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int gallons;
+    int liters;
+    cout << "Enter number of gallons: ";
+    cin >> gallons;
+    liters = gallons * 4;
+    cout << "Liters: " << liters;
+    return 0;
+}
