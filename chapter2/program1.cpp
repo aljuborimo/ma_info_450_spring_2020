5eff754b96442ad00049ea1a2f9a0975c22c18ce
aljuborimo <58051833+aljuborimo@users.noreply.github.com>
2020-01-23 16:44:18 -0500

    Add files via upload

--- chapter2/program1.cpp
+++ chapter2/program1.cpp
@@ -1,1 +1,12 @@
+/*  
+    Mohammed aljubori 
+    Info 450, Professor Fauerbach
+*/
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    cout  << "This is my first C++ program.";
+    return 0;
+}
